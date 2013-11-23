b0e9a03a4109df2bf39c419721ed29de727f8048
Louis Bataillard <info@mobweb.ch>
2013-11-23 14:26:19 +0100

    modified the default "send a friend" email model to append the custom "ref" parameter to any product url sent via this email

---
+++ app/code/local/MobWeb/ReferralCoupon/Model/Sendfriend.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ *
+ * Extending the default Sendfriend model to modify the product URL used
+ * in the recommendation email and include the "ref" parameter
+ *
+ */
+class MobWeb_ReferralCoupon_Model_Sendfriend extends Mage_Sendfriend_Model_Sendfriend
+{
+    public function send()
+    {
+        if ($this->isExceedLimit()){
+            Mage::throwException(Mage::helper('sendfriend')->__('You have exceeded limit of %d sends in an hour', $this->getMaxSendsToFriend()));
+        }
+
+        /* @var $translate Mage_Core_Model_Translate */
+        $translate = Mage::getSingleton('core/translate');
+        $translate->setTranslateInline(false);
+
+        /* @var $mailTemplate Mage_Core_Model_Email_Template */
+        $mailTemplate = Mage::getModel('core/email_template');
+
+        $message = nl2br(htmlspecialchars($this->getSender()->getMessage()));
+        $sender  = array(
+            'name'  => $this->_getHelper()->htmlEscape($this->getSender()->getName()),
+            'email' => $this->_getHelper()->htmlEscape($this->getSender()->getEmail())
+        );
+
+        $mailTemplate->setDesignConfig(array(
+            'area'  => 'frontend',
+            'store' => Mage::app()->getStore()->getId()
+        ));
+
+        // Append the "ref" parameter to the product URL
+        $url = $this->getProduct()->getUrlInStore();
+        if(($customerId = Mage::getSingleton('customer/session')->getCustomer()->getId()) && Mage::getSingleton('customer/session')->isLoggedIn()) {
+            $url .= strpos($url, '?') ? '&ref=' : '?ref=';
+            $url .= $customerId;
+        }
+
+        foreach ($this->getRecipients()->getEmails() as $k => $email) {
+            $name = $this->getRecipients()->getNames($k);
+            $mailTemplate->sendTransactional(
+                $this->getTemplate(),
+                $sender,
+                $email,
+                $name,
+                array(
+                    'name'          => $name,
+                    'email'         => $email,
+                    'product_name'  => $this->getProduct()->getName(),
+                    'product_url'   => $url,
+                    'message'       => $message,
+                    'sender_name'   => $sender['name'],
+                    'sender_email'  => $sender['email'],
+                    'product_image' => Mage::helper('catalog/image')->init($this->getProduct(),
+                        'small_image')->resize(75),
+                )
+            );
+        }
+
+        $translate->setTranslateInline(true);
+        $this->_incrementSentCount();
+
+        return $this;
+    }
+}
